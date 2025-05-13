88c5f6d8a3d89733910c3ef7bfe4c556dc870218
Antonios Monogyios <shiryous@hotmail.com>
2025-05-13 17:17:36 +0300

    Added the models of the entities

---
+++ app/Models/Enrolment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Enrolment extends Model
+{
+    /** @use HasFactory<\Database\Factories\EnrolmentFactory> */
+    use HasFactory;
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
